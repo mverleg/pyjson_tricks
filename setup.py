7ff7f763454bd11568f86532f5f7c2ad2fa21462
mark <mark@rafiki>
2017-01-18 22:33:43 +0100

    v3.7 (decimals, fractions)

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ setup(
 	author_email='mdilligaf@gmail.com',
 	license='Revised BSD License (LICENSE.txt)',
 	keywords=['json', 'numpy', 'OrderedDict', 'comments',],
-	version='3.6.0',
+	version='3.7.0',
 	packages=['json_tricks'],
 	include_package_data=True,
 	zip_safe=False,
