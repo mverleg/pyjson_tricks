562da7c329e2c03a755637cdf57612049f092b57
mark <mark@rafiki>
2017-01-19 19:01:25 +0100

    add utility to optionally pass arguments, to pass flags to encoders that accept it

--- json_tricks/utils.py
+++ json_tricks/utils.py
@@ -11,3 +11,32 @@ class hashodict(OrderedDict):
 		return hash(frozenset(self.items()))
 
 
+try:
+	from inspect import signature
+except ImportError:
+	try:
+		from inspect import getfullargspec
+	except ImportError:
+		from inspect import getargspec
+		def get_arg_names(callable):
+			argspec = getargspec(callable)
+			return set(argspec.args)
+	else:
+		def get_arg_names(callable):
+			argspec = getfullargspec(callable)
+			return set(argspec.args) | set(argspec.kwonlyargs)
+else:
+	def get_arg_names(callable):
+		sig = signature(callable)
+		return set(sig.parameters.keys())
+
+
+def call_with_optional_kwargs(callable, *args, **optional_kwargs):
+	accepted_kwargs = get_arg_names(callable)
+	use_kwargs = {}
+	for key, val in optional_kwargs.items():
+		if key in accepted_kwargs:
+			use_kwargs[key] = val
+	return callable(*args, **use_kwargs)
+	
+
