18281eff0dace8e7dbbdc9df5571ee3f3fe5b4af
Mark <mverleg.noreply@gmail.com>
2019-03-03 00:28:31 +0100

    Release 3.13 version supporting pathlib

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ setup(
 	license='Revised BSD License (LICENSE.txt)',
 	keywords=['json', 'numpy', 'OrderedDict', 'comments', 'pandas', 'pytz',
 		'enum', 'encode', 'decode', 'serialize', 'deserialize'],
-	version='3.12.4',
+	version='3.13.0',
 	packages=['json_tricks'],
 	include_package_data=True,
 	zip_safe=True,
