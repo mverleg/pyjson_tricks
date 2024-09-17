ec536c7888799dff3c7a876141ea910f73618619
Clay Dugo <clay@ramonaoptics.com>
2024-09-17 13:45:44 -0400

    Update setup.py for ro_json

--- setup.py
+++ setup.py
@@ -1,69 +1,58 @@
-# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
 
-from sys import version_info
-import warnings
+with open('README.md', 'r', encoding='utf-8') as fh:
+    readme = fh.read()
 
-from setuptools import setup
-
-with open('README.md', 'r') as fh:
-	readme = fh.read()
-
-# with open('json_tricks/_version.py', 'r') as fh:
-# 	version = fh.read().strip()
 from ro_json._version import VERSION
 
-requires = []
-if version_info < (2, 7, 0):
-	requires.append('ordereddict')
-
-if (version_info[0] == 2 and version_info[1] < 7) or \
-		(version_info[0] == 3 and version_info[1] < 4) or \
-		version_info[0] not in (2, 3):
-	raise warnings.warn('`json_tricks` does not support Python version {}.{}'
-		.format(version_info[0], version_info[1]))
-
 setup(
-	name='json_tricks',
-	description='Extra features for Python\'s JSON: comments, order, numpy, '
-		'pandas, datetimes, and many more! Simple but customizable.',
-	long_description_content_type='text/markdown',
-	long_description=readme,
-	url='https://github.com/mverleg/pyjson_tricks',
-	author='Mark V',
-	maintainer='Mark V',
-	author_email='markv.nl.dev@gmail.com',
-	license='Revised BSD License (LICENSE.txt)',
-	keywords=['json', 'numpy', 'OrderedDict', 'comments', 'pandas', 'pytz',
-		'enum', 'encode', 'decode', 'serialize', 'deserialize'],
-	version=VERSION,
-	packages=['json_tricks'],
-	package_data=dict(
-		json_tricks=['LICENSE.txt', 'README.md', 'VERSION'],
-		# tests=['tests/*.py'],
-	),
-	# include_package_data=True,
-	zip_safe=True,
-	classifiers=[
-		'Development Status :: 5 - Production/Stable',
-		'Development Status :: 6 - Mature',
-		'Intended Audience :: Developers',
-		'Natural Language :: English',
-		'License :: OSI Approved :: BSD License',
-		'Operating System :: OS Independent',
-		'Programming Language :: Python',
-		'Programming Language :: Python :: 2',
-		'Programming Language :: Python :: 2.7',
-		'Programming Language :: Python :: 3',
-		'Programming Language :: Python :: 3.4',
-		'Programming Language :: Python :: 3.5',
-		'Programming Language :: Python :: 3.6',
-		'Programming Language :: Python :: 3.7',
-		'Programming Language :: Python :: 3.8',
-		'Programming Language :: Python :: 3.9',
-		'Programming Language :: Python :: Implementation :: CPython',
-		'Programming Language :: Python :: Implementation :: PyPy',
-		'Topic :: Software Development :: Libraries :: Python Modules',
-		# 'Topic :: Utilities',
-	],
-	install_requires=requires,
+    name='ro_json',
+    version=VERSION,
+    description='Extra features for Python\'s JSON: comments, order, numpy, '
+                'pandas, datetimes, and many more! Simple but customizable.',
+    long_description=readme,
+    long_description_content_type='text/markdown',
+    url='https://github.com/ramonaoptics/ro_json',
+    author='Clay Dugo',
+    author_email='clay@ramonaoptics.com',
+    license='BSD-3-Clause',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3 :: Only',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+    keywords=[
+        'json',
+        'numpy',
+        'OrderedDict',
+        'comments',
+        'pandas',
+        'pytz',
+        'enum',
+        'encode',
+        'decode',
+        'serialize',
+        'deserialize',
+        'roundtrip',
+    ],
+    packages=find_packages(exclude=["tests*"]),
+    include_package_data=True,
+    install_requires=[],
+    python_requires='>=3.10',
+    project_urls={
+        # 'Documentation': 'https://your-package-docs-url',
+        'Source': 'https://github.com/ramonaoptics/ro_json',
+        'Tracker': 'https://github.com/ramonaoptics/ro_json/issues',
+    },
+    license_files=('LICENSE.txt',),
 )
